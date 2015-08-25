b0d0b75a89ed7329caece4aada00e079bc8fa879
Smile4ever <computergeoffrey@zoho.com>
2015-08-25 13:09:23 +0200

    Add DELETE shortcut with proper shortcut handling to close tabs Remove INSERT shortcut (was added by accident)

--- FastDelete/chrome/content/fastdelete.js
+++ FastDelete/chrome/content/fastdelete.js
@@ -1,3 +1,29 @@
+window.addEventListener("keyup", function (event) {
+  if (event.defaultPrevented) {
+    return;
+  }
+
+  switch (event.key) {
+    case "Delete":
+		// todo: implement an options window on what do to when the user presses delete
+		// possible options: delete a page, delete a page immediately, close the tab..
+		
+		// order is important here
+		if(content.document.hasFocus() && window.content.document.activeElement.tagName == "BODY"){
+			deletemw.closetab();
+		}else{
+			return;
+		}
+				
+      break;
+    default:
+      return;
+  }
+
+  // don't allow for double actions for a single event
+  event.preventDefault();
+}, true);
+
 var deletemw = {
 	confirm: function() {
 		var str=window.content.location.href;
