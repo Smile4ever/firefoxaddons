94bbbcef049fae2c4165dd3e48f12ff67eb47a7e
Smile4ever <computergeoffrey@zoho.com>
2015-12-13 20:28:10 +0100

    Delete Tab: release 0.3.2 and fix zoho issue, skip over 0.3.1 for AMO

--- Delete Tab/content/deletetab.js
+++ Delete Tab/content/deletetab.js
@@ -1,13 +1,10 @@
-/*window.addEventListener("keyup", function (event) {
+window.addEventListener("keyup", function (event) {
   if (event.defaultPrevented) {
     return;
   }
 
   switch (event.key) {
     case "Delete":
-		// todo: implement an options window on what do to when the user presses delete
-		// possible options: delete a page, delete a page immediately, close the tab..
-		
 		// order is important here
 		if(content.document.hasFocus() && window.content.document.activeElement.tagName == "BODY"){
 			deletetab.closetab();
@@ -22,12 +19,14 @@
 
   // don't allow for double actions for a single event
   event.preventDefault();
-}, true);*/
+}, true);
 
 
 var deletetab = {
 
 	closetab: function(){
-		gBrowser.removeCurrentTab();
+		if(window.content.location.href.indexOf("zoho.com") == -1){
+			gBrowser.removeCurrentTab();
+		}
 	}
 }
