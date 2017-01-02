7754780f5aaf25b9c20cc546ee1e44c778871d41
Geoffrey De Belie <computergeoffrey@zoho.com>
2017-01-02 10:52:17 +0100

    Translate Now 0.4.0: Rewrite addon to be compatible with e10s New feature: reuse tab, enabled by default

--- Translate Now/index.js
+++ Translate Now/index.js
@@ -2,59 +2,33 @@ var self = require("sdk/self")
 var contextMenu = require("sdk/context-menu")
 var tabs = require("sdk/tabs")
 var simple = require("sdk/simple-prefs")
-var selection = require("sdk/selection")
 var selectedText = ""
+var lastTab;
 
 //the addons icon is a modified version of http://www.flaticon.com/free-icon/translator-tool_69101
 //see their website for licensing information
 
-//source: http://stackoverflow.com/questions/8621044/how-to-get-selected-text-using-the-firefox-add-on-sdk
-function selectionChanged(event){
-	// if focus moves away, the selectionChanged event is fired because the context changes
-	// if selection is null, this means the context is switching away
-	// if out of focus, retain the current selection (if an user wants to use the same selection twice)
-	// this is done because the selectionChanged event doesn't fire when the context is switched back
-    if(selection.text != null){
-	   selectedText = selection.text;
-	}
-}
+var cScript = 'self.on("click", function() {' +
+			  '  var sText = window.getSelection().toString();' +
+              '  self.postMessage(sText);' +
+              '});';
 
-selection.on('select', selectionChanged);
-
+var dest_lang_value;
+var source_lang_value;
+var reuse_tab;
+		
 // a small bit of the code below is from the addon "To Google Translate" (including the icon)
 var menuItem = contextMenu.Item({
     label: "Translate Now",
     image: self.data.url("translate.png"),
     context: contextMenu.SelectionContext(),
-    contentScript: 'self.on("click", function() {' +
-                    '   self.postMessage();' +
-                    '});',
-    onMessage: function() {
-		var dest_lang_value = simple.prefs.destination_language
-		if(dest_lang_value == null){
-			dest_lang_value = "en"
-		}
-		var source_lang_value = simple.prefs.source_language
-		if(source_lang_value == null){
-			source_lang_value = "auto"
-		}
-		if(selectedText == null){
-			var notifications = require("sdk/notifications");
-			notifications.notify({
-			  title: "Translate Now",
-			  text: "Try another selection",
-			  data: "OK",
-			  onClick: function (data) {
-			  }
-			});
-		}else{
-			var newText = selectedText //.replace("%", "")
-			newText = encodeURIComponent(newText)
-			newText = newText.replace("%25", "")
-			newText = newText.replace("%C2%A0", " ")
-			tabs.open("http://translate.google.com/#" + source_lang_value + "/" + dest_lang_value + "/" + newText)
-		}
+    contentScript: cScript,
+    onMessage: function(sText) {
+		dest_lang_value = simple.prefs.destination_language;
+		source_lang_value = simple.prefs.source_language;
+		reuse_tab = simple.prefs.reuse_tab;
 
+		doClick(sText, "translate");
     }
 });
 
@@ -63,11 +37,43 @@ var menuItem2 = contextMenu.Item({
     label: "Speak with Google Translate Voice",
     image: self.data.url("mic.png"),
     context: contextMenu.SelectionContext(),
-    contentScript: 'self.on("click", function() {'+
-                    '   self.postMessage();' +
-                    '});',
-    onMessage: function() {
-		if(selectedText.length > 93){
+    contentScript: cScript,
+    onMessage: function(sText) {
+		reuse_tab = simple.prefs.reuse_tab;
+		
+		doClick(sText, "speak");
+    }
+});
+
+var openTab = function(url){
+	if(lastTab != null && reuse_tab){
+		lastTab.url = url;
+		lastTab.activate();
+		return;
+	}
+	tabs.open({
+		url: url,
+		onReady: function(tab) {
+			lastTab = tab;
+		}
+	});
+}
+
+var doClick = function(sText, action){
+	if(sText != "" || sText == null){
+		selectedText = sText;
+	}
+	if(selectedText == null){
+		var notifications = require("sdk/notifications");
+		notifications.notify({
+		  title: "Translate Now",
+		  text: "Try another selection",
+		  data: "OK",
+		  onClick: function (data) {
+		  }
+		});
+	}else{
+		if(selectedText.length > 93 && action == "speak"){
 			//Selected text is too long
 			var notifications = require("sdk/notifications");
 			notifications.notify({
@@ -77,9 +83,16 @@ var menuItem2 = contextMenu.Item({
 			  onClick: function (data) {
 			  }
 			});
-			
 		}else{
-		    tabs.open("http://translate.google.com/translate_tts?tl=en&q=" + encodeURIComponent(selectedText))
+			var newText = selectedText;
+			newText = encodeURIComponent(newText);
+			newText = newText.replace("%25", "");
+			newText = newText.replace("%C2%A0", " ");
+			if(action == "speak"){
+				openTab("http://translate.google.com/translate_tts?tl=en&q=" + newText);
+			}else{
+				openTab("http://translate.google.com/#" + source_lang_value + "/" + dest_lang_value + "/" + newText);
+			}
 		}
-    }
-});
+	}
+}
