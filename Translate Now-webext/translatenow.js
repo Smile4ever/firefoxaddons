316e1dc558349d1a32e9bac83a823fa9ba18cec9
Smile4ever <computergeoffrey@zoho.com>
2017-05-08 19:09:59 +0200

    Translate Now-webext 2.0.2: fix iframe selection

--- Translate Now-webext/translatenow.js
+++ Translate Now-webext/translatenow.js
@@ -27,42 +27,50 @@ function getSelection() {
 		// I don't trust the code above, make sure we return window.getSelection() at all times when there is an error with the code above
 	}
 	
+	var normalSelection = window.getSelection().toString();
+	if(normalSelection != "") return normalSelection;
 	
 	/// Code from Get Archive
+	// Test URL: https://archive.is/2013.07.03-031317/http://neppi.blog.shinobi.jp/ねっぴーのこと/恩師増田宏三先生を悼む	
 	var frameIdentifiers = ["iframe", "frame"];
-	var i = 0;
-	
 	try{
+		var i = 0;
 		for(i = 0; i < frameIdentifiers.length; i++){
 			var frames = document.getElementsByTagName(frameIdentifiers[i]);
-			var i = 0;
-			//console.log("number of frames: " + frames.length);
-			if(frames.length > 0){
-				for(i = 0; i < frames.length; i++){
-					try{
-						var frame = frames[i];
-						var idoc = frame.contentDocument || frame.contentWindow.document;
-						var frameselection = idoc.getSelection();
-						if(frameselection == null){
-							continue;
-						}
-						
-						if(frameselection.toString().length > 0){
-							//console.log("translatenow.js returning (i)frame selection");
-							return frameselection.toString();
-						}
-					}catch(innerex){
-						if(frame.getAttribute("src").indexOf("google") == -1 && frame.getAttribute("src").indexOf("facebook") == -1 && frame.getAttribute("src").indexOf("twitter") == -1){
-							//console.log("CROSS-DOMAIN IFRAME on URL " + frame.getAttribute("src"));
-						}
+			console.log("number of frames: " + frames.length);
+			
+			if(frames.length == 0){
+				continue;
+			}
+			
+			var j = 0;
+			for(j = 0; j < frames.length; j++){
+				var frame = frames[j];
+				if(frame.getAttribute("src").indexOf("google") == -1 && frame.getAttribute("src").indexOf("facebook") == -1 && frame.getAttribute("src").indexOf("twitter") == -1){
+					continue;
+				}
+				try{
+					var frame = frames[j];
+					var idoc = frame.contentDocument || frame.contentWindow.document;
+					var frameselection = idoc.getSelection();
+					if(frameselection == null){
+						continue;
 					}
+					
+					if(frameselection.toString().length > 0){
+						//console.log("translatenow.js returning (i)frame selection");
+						return frameselection.toString();
+					}
+				}catch(innerex){
+					//console.log("CROSS-DOMAIN IFRAME on URL " + frame.getAttribute("src"));
 				}
 			}
+			
 		}
 	}catch(ex){
-		// I don't trust the code above, make sure we return window.getSelection() at all times when there is an error with the code above
+		// I don't trust the code above, return an empty string at all times when there is an error with the code above
 	}
 	/// End of code from Get Archive
 	
-    return window.getSelection().toString();
+    return "";
 }
