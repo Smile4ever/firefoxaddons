028ce90b2aa79c32b13225fad9a30620d4a6fd66
Smile4ever <computergeoffrey@zoho.com>
2017-11-23 20:12:50 +0100

    Double Click On Page Closes Tab-webext 0.1.1: cleanup

--- Double Click On Page Closes Tab-webext/CHANGELOG.md
+++ Double Click On Page Closes Tab-webext/CHANGELOG.md
@@ -1,3 +1,7 @@
+0.1.1
+=====
+* Cleanup
+
 0.1.0
 =====
 * First public release
--- Double Click On Page Closes Tab-webext/background.js
+++ Double Click On Page Closes Tab-webext/background.js
@@ -5,9 +5,6 @@ browser.runtime.onMessage.addListener(function(message) {
 		case "closeTab":
 			closeTab();
 			break;
-		case "notify":
-			notify(message.data);
-			break;
 		case "onError":
 			onError(message.data);
 			break;
@@ -16,19 +13,6 @@ browser.runtime.onMessage.addListener(function(message) {
 	}
 });
 
-// See also https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/Tabs/sendMessage
-function sendMessage(action, data){
-	function logTabs(tabs) {
-		for (tab of tabs) {
-			browser.tabs.sendMessage(tab.id, {"action": action, "data": data}).catch(function(){
-				onError("failed to execute " + action + "with data " + data);
-			});
-		}
-	}
-
-	browser.tabs.query({currentWindow: true, active: true}).then(logTabs, onError);
-}
-
 /// Tab functions
 function closeTab(){
 	browser.tabs.query({currentWindow: true, active: true}).then(onSuccess, onError);
@@ -36,26 +20,10 @@ function closeTab(){
 	function onSuccess(activeTabs) {
 		var activeTab = activeTabs[0];
 		browser.tabs.remove(activeTab.id);
-		//onDebug("Double Click On Page Closes Tab " + activeTab.url);
 	}
 }
 
 /// Helper functions
 function onError(error) {
 	console.error(`${error}`);
-}
-
-function notify(message){
-	var title = "Double Click On Page Closes Tab";
-	if(typeof message === "object"){
-		title = message.title;
-		message = message.message;
-	}
-	
-	browser.notifications.create({
-		type: "basic",
-		iconUrl: browser.extension.getURL("icons/closetabicon.png"),
-		title: title,
-		message: message
-	});
 }
